ad9084613d13f36408c714f2d238327eeac642d4
Brian Peiris <brianpeiris@gmail.com>
2017-04-14 05:58:29 -0700

    formatting

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -43,8 +43,10 @@ AFRAME.registerComponent('scoreboard', {
       <a-entity>
         <a-entity n-text={properties({text: myScore})} position="0 0.2 -1.7" n-cockpit-parent></a-entity>
         {sortedUsers.map((user, i) =>
-          <a-entity><a-entity n-text={properties({text: user.displayName})} position={`0 ${i} 0`}></a-entity>
-          <a-entity n-text={properties({text: Math.min(user.score, this.data.max)})} position={`4 ${i} 0`}></a-entity></a-entity>
+          <a-entity>
+            <a-entity n-text={properties({text: user.displayName})} position={`0 ${i} 0`}></a-entity>
+            <a-entity n-text={properties({text: Math.min(user.score, this.data.max)})} position={`4 ${i} 0`}></a-entity>
+          </a-entity>
         )}
       </a-entity>,
       this.el
