ec5ce00909f43dece12a5430d882a0071fb781f3
Brian Peiris <brianpeiris@gmail.com>
2017-04-14 05:35:08 -0700

    Render score board and personal score

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,3 +1,6 @@
+function properties(obj) {
+  return _(obj).toPairs().map(([key, value]) => `${key}: ${value}`)
+}
 AFRAME.registerComponent('scoreboard', {
   dependencies: ['sync'],
   schema: {
@@ -5,14 +8,20 @@ AFRAME.registerComponent('scoreboard', {
     filter: {type: 'string'}
   },
 	init: function () {
-    var syncSys = this.el.components.sync.syncSys;
+    this.syncSys = this.el.components.sync.syncSys;
+    if (this.syncSys.isConnected) {
+      this.setup();
+    }
+    else {
+      this.el.sceneEl.addEventListener('connected', this.setup.bind(this));
+    }
+  },
+  setup() {
     var dataRef = this.el.components.sync.dataRef;
-    var userRef = dataRef.child('users').child(syncSys.userInfo.userId)
+    this.userId = this.syncSys.userInfo.userId;
+    var userRef = dataRef.child('users').child(this.userId);
 
-    userRef.child('info').set({
-      userId: syncSys.userInfo.userId,
-      displayName: syncSys.userInfo.displayName,
-    });
+    userRef.child('displayName').set(this.syncSys.userInfo.displayName);
 
     this.score = userRef.child('score');
 
@@ -21,12 +30,21 @@ AFRAME.registerComponent('scoreboard', {
 	},
   incrementScore: function (event) {
     if (!event.target.matches(this.data.filter)) { return; }
-    this.score.transaction(function (val) {
-      return val + 1;
-    });
+    this.score.transaction((val) => val + 1);
   },
   renderScores: function (snapshot) {
-    var val = snapshot.val();
-    console.log(JSON.stringify(val));
+    var users = snapshot.val();
+    var sortedUsers = _(users).values().filter('score').sortBy('score').value();
+    var myScore = users[this.userId].score || '';
+    ReactDOM.render(
+      <a-entity>
+        <a-entity n-text={properties({text: myScore})} position="0 0.2 -1.7" n-cockpit-parent></a-entity>
+        {sortedUsers.map((user, i) =>
+          <a-entity><a-entity n-text={properties({text: user.displayName})} position={`0 ${i} 0`}></a-entity>
+          <a-entity n-text={properties({text: user.score})} position={`4 ${i} 0`}></a-entity></a-entity>
+        )}
+      </a-entity>,
+      this.el
+    );
   }
 });
